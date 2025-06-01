bc6e7f4d9f005bedbbd32000cf71d30a29fc59fa
deepika1998kala@gmail.com <deepika1998kala@gmail.com>
2025-06-01 12:54:25 +0530

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
 import { Server } from "socket.io";
-import { io, userSocketMap } from '../server.js'; // adjust relative path
+
 
 // dotenv.config();
 
